0ed36bf3d97059ed6ee230823c2f3e8f89cd3cf5
MAleemH <aleemhashmi4321@gmail.com>
2023-06-10 13:16:05 +0500

    Added required pages with dummy data

---
+++ admin/services.php
@@ -0,0 +1,56 @@
+<!-- head and sidebar -->
+<?php
+
+include '../includes/admin_head.php';
+include '../includes/admin_sidebar.php';
+?>
+
+<section class="home-section">
+    <!-- navbar -->
+    <?php
+    include '../includes/admin_navbar.php';
+    ?>
+
+    <div class="home-content">
+
+        <div class="other-details">
+            <h3>Services</h3>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur sit vel animi nulla in,
+                blanditiis
+                placeat repellat tempore voluptas reprehenderit laborum sint minus velit facilis natus sequi vero
+                perferendis?
+                Repellat reprehenderit quibusdam voluptate non recusandae hic reiciendis blanditiis consequatur sit
+                dolorum est
+                fugiat, dolores magnam doloremque illum, repudiandae architecto saepe! Consequuntur, ipsam. Ut animi,
+                ipsa
+                repellat nisi obcaecati totam!
+                Deleniti excepturi commodi inventore sapiente cupiditate alias autem amet illum incidunt harum tempore
+                quam,
+                assumenda quibusdam culpa ullam cum numquam esse recusandae sed laborum nulla, repellat vitae qui. In,
+                velit!
+                Illum excepturi tempora esse debitis ea veniam quibusdam distinctio impedit ratione sed tempore
+                blanditiis quae
+                hic magnam, aperiam quisquam, nesciunt a modi eligendi, voluptate voluptates pariatur rem. Debitis, qui
+                dicta.
+                Distinctio sit illo dolorem neque rem modi unde optio doloribus aperiam, itaque iste officia maxime eos
+                inventore? Illo, earum repellat eligendi, nemo numquam similique dolorem non quaerat nihil, labore
+                rerum!
+                Veritatis repellat aspernatur sit magni, soluta odio. Natus voluptatibus eos, aliquam nisi cumque
+                voluptas qui
+                voluptatum voluptates. Atque earum inventore debitis ipsa, nulla esse, eos dignissimos placeat
+                doloremque cum
+                officia.
+                Quia cum numquam consectetur sunt! Delectus quibusdam, exercitationem doloremque libero voluptatem odit?
+                Perferendis natus id excepturi asperiores, veritatis officia, esse accusamus doloremque reprehenderit
+                voluptate
+                sunt alias praesentium aut labore cupiditate.
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- navbar -->
+<?php
+include '../includes/admin_footer.php';
+?>
