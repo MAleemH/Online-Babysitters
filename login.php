2ae502f9e2ebf4819528793b453167c093027573
MAleemH <aleemhashmi4321@gmail.com>
2023-06-21 17:37:44 +0500

    Modified login page

--- login.php
+++ login.php
@@ -9,17 +9,17 @@ include './includes/header.php';
 <main>
   <!-- login -->
   <section id="login">
-    <h3 class="text-white fs-1">Login</h3>
+    <h2 class="text-black">LOGIN</h2>
     <form action="" method="post" class="login-form">
       <div class="mb-3">
-        <label for="" class="form-label">Email</label>
-        <input type="email" class="form-control" name="" id="" placeholder="abc@mail.com" />
+        <label class="form-label fw-bold">Email</label>
+        <input type="email" class="form-control" name="" placeholder="abc@mail.com" />
       </div>
-      <div class="mb-3">
-        <label for="" class="form-label">Password</label>
-        <input type="password" class="form-control" name="" id="" placeholder="" />
+      <div class="mb-4">
+        <label class="form-label fw-bold">Password</label>
+        <input type="password" class="form-control" name="" />
       </div>
-      <button type="button" class="btn btn-primary">Login</button>
+      <button type="button" class="btn btn-primary w-100">Login</button>
     </form>
   </section>
 </main>
