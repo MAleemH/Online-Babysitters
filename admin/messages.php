a1da4c6c6ccce3216a5e029a029046cac72b17a5
MAleemH <aleemhashmi4321@gmail.com>
2023-06-24 14:21:03 +0500

    Contact form enabled

--- admin/messages.php
+++ admin/messages.php
@@ -15,35 +15,54 @@ include '../includes/admin_sidebar.php';
 
         <div class="other-details">
             <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium pariatur sit vel animi nulla in,
-                blanditiis
-                placeat repellat tempore voluptas reprehenderit laborum sint minus velit facilis natus sequi vero
-                perferendis?
-                Repellat reprehenderit quibusdam voluptate non recusandae hic reiciendis blanditiis consequatur sit
-                dolorum est
-                fugiat, dolores magnam doloremque illum, repudiandae architecto saepe! Consequuntur, ipsam. Ut animi,
-                ipsa
-                repellat nisi obcaecati totam!
-                Deleniti excepturi commodi inventore sapiente cupiditate alias autem amet illum incidunt harum tempore
-                quam,
-                assumenda quibusdam culpa ullam cum numquam esse recusandae sed laborum nulla, repellat vitae qui. In,
-                velit!
-                Illum excepturi tempora esse debitis ea veniam quibusdam distinctio impedit ratione sed tempore
-                blanditiis quae
-                hic magnam, aperiam quisquam, nesciunt a modi eligendi, voluptate voluptates pariatur rem. Debitis, qui
-                dicta.
-                Distinctio sit illo dolorem neque rem modi unde optio doloribus aperiam, itaque iste officia maxime eos
-                inventore? Illo, earum repellat eligendi, nemo numquam similique dolorem non quaerat nihil, labore
-                rerum!
-                Veritatis repellat aspernatur sit magni, soluta odio. Natus voluptatibus eos, aliquam nisi cumque
-                voluptas qui
-                voluptatum voluptates. Atque earum inventore debitis ipsa, nulla esse, eos dignissimos placeat
-                doloremque cum
-                officia.
-                Quia cum numquam consectetur sunt! Delectus quibusdam, exercitationem doloremque libero voluptatem odit?
-                Perferendis natus id excepturi asperiores, veritatis officia, esse accusamus doloremque reprehenderit
-                voluptate
-                sunt alias praesentium aut labore cupiditate.
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Message</th>
+                            <th scope="col">Date & Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+
+                        $query = "SELECT * FROM messages";
+                        $query_conn = mysqli_query($connection, $query);
+
+                        while ($result = mysqli_fetch_assoc($query_conn)) {
+                            $message_id = $result['id'];
+                            $name = $result['name'];
+                            $email = $result['email'];
+                            $message = $result['message'];
+                            $date_time = $result['created_at'];
+
+                            ?>
+                            <tr>
+                                <th scope="row">
+                                    <?php echo $message_id; ?>
+                                </th>
+                                <td>
+                                    <?php echo $name; ?>
+                                </td>
+                                <td>
+                                    <?php echo $email; ?>
+                                </td>
+                                <td>
+                                    <?php echo $message; ?>
+                                </td>
+                                <td>
+                                    <?php echo $date_time; ?>
+                                </td>
+                            </tr>
+                            <?php
+
+                        }
+
+                        ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- contact.php
+++ contact.php
@@ -10,42 +10,53 @@ include './includes/header.php';
   <section id="contact">
     <!-- contact us title -->
     <div class="row m-3">
-      <div class="col-12 text-uppercase text-center">
-        <h2 class="text-black">Contact Us</h2>
+      <div class="col-12 text-uppercase text-center text-white">
+        <h2>Contact Us</h2>
       </div>
     </div>
     <div class="row mt-5">
+      <?php
+        $query = "SELECT * FROM contact";
+        $query_conn = mysqli_query($connection, $query);
+
+        while ($result = mysqli_fetch_assoc($query_conn)) {
+          $contact_id = $result['id'];
+          $email = $result['email'];
+          $contact_number = $result['contact_number'];
+          $address = $result['address'];
+        }
+      ?>
       <div class="col-6">
         <div class="contact-info text-white fs-5 d-flex flex-column justify-content-center gap-5">
           <div class="d-flex flex-row align-items-center">
             <i class="fa-solid fa-phone"></i>
-            <p class="m-0 ms-4">+923334455666</p>
+            <p class="m-0 ms-4"><?php echo $contact_number; ?></p>
           </div>
           <div class="d-flex flex-row align-items-center">
             <i class="fa-solid fa-envelope"></i>
-            <p class="m-0 ms-4">contact@onlinebabysitters.com</p>
+            <p class="m-0 ms-4"><?php echo $email; ?></p>
           </div>
           <div class="d-flex flex-row align-items-center">
             <i class="fa-solid fa-location-dot"></i>
-            <p class="m-0 ms-4 ps-1">abc Street, xyz City, Pakistan</p>
+            <p class="m-0 ms-4 ps-1"><?php echo $address; ?></p>
           </div>
         </div>
       </div>
       <div class="col-6">
-        <form action="" method="post">
+        <form action="queries/contact_form.php" method="POST">
           <div class="mb-3">
-            <label for="" class="form-label fw-bold">Name</label>
-            <input type="text" name="" class="form-control" placeholder="Name">
+            <label class="form-label fw-bold text-white">Name</label>
+            <input type="text" name="name" class="form-control" placeholder="Name" required>
           </div>
           <div class="mb-3">
-            <label for="" class="form-label fw-bold">Email</label>
-            <input type="email" name="" class="form-control" placeholder="abc@gmail.com">
+            <label class="form-label fw-bold text-white">Email</label>
+            <input type="email" name="email" class="form-control" placeholder="abc@gmail.com" required>
           </div>
           <div class="mb-3">
-            <label for="" class="form-label fw-bold">Message</label>
-            <textarea class="form-control" name="" rows="9" placeholder="Type your message here..."></textarea>
+            <label class="form-label fw-bold text-white">Message</label>
+            <textarea class="form-control" name="message" placeholder="Type your message here..." style="height: 240px" required></textarea>
           </div>
-          <button class="btn btn-primary" type="submit">Submit</button>
+          <button class="btn btn-primary" type="submit" name='submit'>Submit</button>
         </form>
       </div>
     </div>
